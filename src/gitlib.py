baf68b073e3d11585685962b23868200c8ce600e
egor.dranischnikow <egor.dranischnikow@googlemail.com>
2016-07-22 21:30:31 +0200

    putting GitRepository into its own file

--- src/gitlib.py
+++ src/gitlib.py
@@ -1,53 +1,8 @@
-import sh
+
+from gwlib.grepo import GitRepository
 
 class GitWalkerError(Exception):
-    pass
-
-from gwlib.gcommit import GitCommit
-
-class GitRepository:
-    def __init__(self, path):
-        self.git_rep=["-C", path]
-    
-    
-    def __run_command(self, command, capture_output=False):
-        #to see why _tty_out=False go to stackoverflow.com/a/36252138/325365 
-        call=sh.git(self.git_rep+command, _tty_out=False)
-        if capture_output:
-            #strip to get rid of \n
-            #str to get read of u'...'
-            return [str(line.strip()) for line in call]
-        
-    def get_commit(self, commit_hash):
-        return GitCommit(commit_hash, self)
-    
-    
-    def get_branch_commits(self, branch_name):
-        command=["log", branch_name, "^master", "--format=%H"]
-        commit_hashes=self.__run_command(command, capture_output=True)
-        return [self.get_commit(commit_hash) for commit_hash in reversed(commit_hashes)]
-        
-            
-    def checkout(self, commit_alias):
-        self.__run_command(["checkout", commit_alias])
-       
-        
-    def get_commit_titel(self, commit_hash):
-        command=["log", "--format=%s", "-n", "1", commit_hash]
-        return self.__run_command(command, capture_output=True)[0]
-        
-        
-    def get_status(self):
-        command=["status"]
-        return self.__run_command(command, capture_output=True)
-        
-        
-    def get_parent_commit(self, commit_hash):
-        command=["log", "--format=%P", "-n", "1", commit_hash]
-        hash_value=self.__run_command(command, capture_output=True)
-        return self.get_commit(hash_value[0])
-           
-
+    pass           
         
        
 class GitBranchRunner:
