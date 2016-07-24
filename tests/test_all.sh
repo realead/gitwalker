5468ca04459754e9ebbe587994ad444fa0fd4879
egor.dranischnikow <egor.dranischnikow@googlemail.com>
2016-07-24 20:03:36 +0200

    separating tests in unit tests and front end tests

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,9 +1,9 @@
 
 
+echo "TESTING FRONT END:"
+sh test_front_end.sh
+
+echo "\n\n\nRUNNING UNIT TESTS:"
 python -m unittest discover -s unittests
 
 
-#verify branch:
-GIT_WALKER="python2.7 ../src/gitwalker.py"
-GIT_REP="../testrep"
-$GIT_WALKER -g $GIT_REP -s scripts/dreamer_not_in_fileB.sh
